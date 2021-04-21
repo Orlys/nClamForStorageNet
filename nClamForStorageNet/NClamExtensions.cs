f287a6cd498ed1595a489e6c32798f89e9d73ff4
Orlys <contact.orlys@gmail.com>
2021-04-21 11:49:51 +0800

    Add project files.

---
+++ nClamForStorageNet/NClamExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Storage.Net.Blobs
+{
+    using nClam;
+
+    using Storage.Net.Blobs.Sinks;
+
+    using System;
+    using System.Net;
+
+    public static class NClamExtensions
+    {
+        public static IBlobStorage WithNClam(this IBlobStorage blobStorage, IPEndPoint endpoint, Action<ClamScanResult> virusDetectedCallback = null)
+            => WithNClam(blobStorage, () => new ClamClient(endpoint.Address, endpoint.Port), virusDetectedCallback);
+
+        public static IBlobStorage WithNClam(this IBlobStorage blobStorage, Func<IClamClient> clientFactory, Action<ClamScanResult> virusDetectedCallback = null)
+            => blobStorage.WithSinks(new NClamSink(clientFactory, virusDetectedCallback)); 
+    }
+
+}
---
+++ nClamForStorageNet/NClamSink.cs
@@ -0,0 +1,76 @@
+﻿namespace Storage.Net.Blobs.Sinks
+{
+    using nClam; 
+
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    public class NClamSink : ITransformSink
+    {
+        private static T Forget<T>(Task<T> task)
+        {
+            return task.ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        private class EmptyStream : MemoryStream
+        {
+            internal EmptyStream() : base(Array.Empty<byte>())
+            {
+            }
+        }
+
+        protected Func<IClamClient> NClamClientFactory { get; }
+        protected Action<ClamScanResult> VirusDetectedCallback { get; }
+
+        public NClamSink(Func<IClamClient> clientFactory, Action<ClamScanResult> virusDetectedCallback)
+        {
+            this.NClamClientFactory = clientFactory;
+            this.VirusDetectedCallback = virusDetectedCallback;
+        }
+
+        public virtual Stream OpenReadStream(string fullPath, Stream parentStream)
+        {
+            return ScanVirus(fullPath, parentStream);
+        }
+
+        protected virtual Stream ScanVirus(string fullPath, Stream parentStream)
+        {
+            var client = NClamClientFactory();
+            if (Forget(client.TryPingAsync()))
+            {
+                var stream = default(Stream);
+                if (!parentStream.CanSeek)
+                {
+                    stream = new MemoryStream();
+                    parentStream.CopyTo(stream);
+                }
+                else
+                {
+                    stream = parentStream;
+                }
+                stream.Seek(0, SeekOrigin.Begin);
+
+                var scanResult = Forget(client.SendAndScanFileAsync(stream));
+                if (!scanResult.Result.Equals(ClamScanResults.Clean))
+                {
+                    VirusDetectedCallback?.Invoke(scanResult);
+                    return new EmptyStream();
+                }
+                else
+                {
+                    stream.Seek(0, SeekOrigin.Begin);
+                    return stream;
+                }
+            }
+
+            return parentStream;
+        }
+
+        public virtual Stream OpenWriteStream(string fullPath, Stream parentStream)
+        {
+            return ScanVirus(fullPath, parentStream);
+        }
+    }
+
+}
